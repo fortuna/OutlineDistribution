d8ae411b869e4f2dfb85b959f0abf239de10f3fc
Vinicius Fortuna <fortuna@users.noreply.github.com>
2024-02-14 02:21:10 -0500

    Serve Dynamic Keys

--- Code.js
+++ Code.js
@@ -3,17 +3,77 @@
 // - Use Spreadsheet as backend
 
 const MANAGEMENT_API_URL = PropertiesService.getScriptProperties().getProperty('API_URL');
+const TOKEN_SECRET = PropertiesService.getScriptProperties().getProperty('TOKEN_SECRET');
 
-function doGet() {
-    // Use Session.getActiveUser().getEmail()
+function doGet(e) {
+    if (e.pathInfo && e.pathInfo.startsWith("access_key/")) {
+      return serveDynamicKey(e.pathInfo.substr("access_key/".length))
+    }
+    return serveHome();
+}
+
+function serveHome() {
     const template = HtmlService.createTemplateFromFile('Index');
-    template.email = Session.getActiveUser().getEmail();
-    const accessKey = getAccessKey();
-    template.accessKey = accessKey?.accessUrl || '';
-    template.bytes30d = getBytes30d();
     const htmlContent = template.evaluate();
     htmlContent.addMetaTag('viewport', 'width=device-width, initial-scale=1');
     return htmlContent;
+}
+
+function createDeviceKey() {
+  const deviceId = Utilities.getUuid();
+  const now = new Date();
+  const encodedPayload = Utilities.base64EncodeWebSafe(JSON.stringify({
+    "sub": deviceId,
+    "iat": now.valueOf(),
+    "exp": now.setMonth(now.getMonth() + 1).valueOf(),
+  }));
+  const signature = Utilities.base64EncodeWebSafe(
+    Utilities.computeHmacSha256Signature(encodedPayload, TOKEN_SECRET)
+  );
+  const token = encodedPayload + '.' + signature;
+  const deviceKey = `${ScriptApp.getService().getUrl()}/access_key/${token}`.replace(/^https:/, 'ssconf:');
+  return deviceKey;
+}
+
+function serveDynamicKey(token) {
+  // const token = "eyJzdWIiOiI3Yzc3N2ZjMC0zYTFkLTQ1YTItYWE2NC1hOWYzY2QwM2I1NTUiLCJpYXQiOjE3MDc4OTA1NDIzODMsImV4cCI6MTcxMDM5MjU0MjM4M30=.hgofD3_Nufeb2Pp3_rAvgPxUg-EwxxqTzO6U3wBjU5o=";
+  const parts = token.split('.');
+  if (parts.length !== 2) {
+    throw new Error('The token is invalid: bad format');
+  }
+  const [encodedPayload, presentedSignature] = parts;
+  const expectedSignature = Utilities.base64EncodeWebSafe(
+    Utilities.computeHmacSha256Signature(encodedPayload, TOKEN_SECRET)
+  );
+  if (expectedSignature != presentedSignature) {
+    throw new Error('The token is invalid: bad signature');
+  }
+  const payloadBytes = Utilities.base64DecodeWebSafe(encodedPayload);
+  const payload = JSON.parse(Utilities.newBlob(payloadBytes).getDataAsString())
+  Logger.log(payload);
+  if (Date.now() >= payload.exp) {
+    throw new Error('The token is invalid: expired');
+  }
+  const deviceId = payload.sub;
+
+  // Use lock to make sure deletions + creation is atomic.
+  const lock = LockService.getScriptLock();
+  lock.waitLock(5000);
+  var responseText
+  try {
+    try {
+      apiFetch('access-keys/' + deviceId, {method: 'delete'});
+    } catch {}
+    // https://redocly.github.io/redoc/?url=https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/shadowbox/server/api.yml#tag/Access-Key/paths/~1access-keys/put
+    const response = apiFetch('access-keys/' + deviceId, {method: 'put'});
+    const responseText = response.getContentText();
+
+  } finally {
+    lock.releaseLock();
+  }
+  const keyObj = JSON.parse(responseText);
+  const sessionKey = keyObj.accessUrl;
+  return ContentService.createTextOutput(sessionKey).setMimeType(ContentService.MimeType.TEXT);
 }
 
 // For API details, see https://redocly.github.io/redoc/?url=https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/shadowbox/server/api.yml
@@ -25,75 +85,3 @@ function apiFetch(path, options) {
   });
 }
 
-function getUserId() {
-  return PropertiesService.getUserProperties().getProperty('USER_ID');
-}
-
-function getAccessKey() {
-  const id = getUserId();
-  if (!id) {
-    return undefined;
-  }
-  const cache = CacheService.getUserCache();
-  let keyStr = cache.get("USER_KEY");
-  if (keyStr) {
-    try {
-      return JSON.parse(keyStr);
-    } catch {
-      cache.remove("USER_KEY");
-    }
-  }
-  // https://redocly.github.io/redoc/?url=https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/shadowbox/server/api.yml#tag/Access-Key/paths/~1access-keys/get
-  const response = apiFetch('access-keys', {method: 'get'});
-  const responseJson = JSON.parse(response.getContentText());
-  for (key of responseJson.accessKeys) {
-    if (key.id === id) {
-      cache.put("USER_KEY", JSON.stringify(key), 21600);
-      return key;
-    }    
-  }
-  return undefined;
-}
-
-function getBytes30d() {
-    const keyId = getUserId();
-    const response = apiFetch('metrics/transfer', {method: 'get'});
-
-    if (response.getResponseCode() != 200) {
-      return -1
-    }
-    const usage = JSON.parse(response.getContentText()).bytesTransferredByUserId;
-    return usage[keyId] ?? 0;
-}
-
-function deleteUserKey() {
-    const keyId = getUserId();
-    // https://redocly.github.io/redoc/?url=https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/shadowbox/server/api.yml#tag/Access-Key/paths/~1access-keys~1%7Bid%7D/delete
-    const response = apiFetch(`access-keys/${keyId}`, {method: 'delete'});
-    if (response.getResponseCode() === 200 || response.getResponseCode() === 404) {
-      PropertiesService.getUserProperties().deleteProperty('USER_ID');
-      CacheService.getUserCache().remove('USER_KEY');
-    }
-}
-
-function createUserKey() {
-    // TODO: Use LockService
-    const lock = LockService.getUserLock();
-    lock.waitLock(5000);
-    try {
-      // https://redocly.github.io/redoc/?url=https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/shadowbox/server/api.yml#tag/Access-Key/paths/~1access-keys/post
-      const response = apiFetch('access-keys', {method: 'post'});
-      const keyStr = response.getContentText();
-      const keyObj = JSON.parse(keyStr);
-
-      if (keyObj) {
-          PropertiesService.getUserProperties().setProperty('USER_ID', keyObj.id);
-          CacheService.getUserCache().put("USER_KEY", keyStr, 21600);
-          return keyObj.accessUrl;
-      } else {
-          throw new Error('Failed to create access key');
-      }
-    } finally {
-      lock.releaseLock();
-    }
-}
